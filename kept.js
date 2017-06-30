e592c048a0115b62efb2756cdb4f73eed460b91f
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-30 19:02:11 +0800

    Updated dependency.

--- kept.js
+++ kept.js
@@ -32,10 +32,10 @@
 			"file": "kept.js",
 			"module": "kept",
 			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
 			"contributors": [
 				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
 			],
-			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/kept.git",
 			"test": "kept-test.js",
 			"global": true
